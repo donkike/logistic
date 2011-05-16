efeb082dec9dc101c82c4429a5ab567565e1000d
aremun19 <dmunera119@gmail.com>
2011-05-15 20:37:45 -0500

    views

--- app/helpers/vmodules_helper.rb
+++ app/helpers/vmodules_helper.rb
@@ -1,4 +1,11 @@
 module VmodulesHelper
+
+  def main_information vmodule
+    content_tag(:div, :class => 'main_information') do
+      content_tag(:strong, (t(:vmodule_efficiency))) <<
+      content_tag(:strong, (vmodule.vmodule_efficiency))
+    end
+  end
   def small_user_information user
     content_tag(:td,:class => 'small_information', :id => 'small') do
       content_tag(:div,(link_to user.username, vmodule_user_path(user.vmodule,user)))
