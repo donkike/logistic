608462f46105eb6e72b18ce4bc09fa1c3e602517
Enrique Arango Lyons <karlysw@gmail.com>
2011-06-23 10:58:26 +0200

    fix in case of resumed activity

--- app/controllers/report_activity_users_controller.rb
+++ app/controllers/report_activity_users_controller.rb
@@ -12,11 +12,10 @@ class ReportActivityUsersController < ApplicationController
   end
 
   def create
-    @report_activity_user = ReportActivityUser.new(params[:report_activity_user])
-    if @report_activity_user.save
-      #redirect_to @report_activity_user, :notice => "Successfully created report activity user."
+    @report_activity_user = ReportActivityUser.find_or_create_by_user_id_and_activity_id(params[:report_activity_user])
+    if @report_activity_user.save      
       respond_to do |format|
-        #format.html redirect_to(@report_activity_user, :notice => "Successfully created report activity user.") 
+        format.html { redirect_to(@report_activity_user, :notice => "Successfully created report activity user.") } 
         format.xml { render :xml => @report_activity_user.to_xml }
       end
     else
@@ -30,10 +29,9 @@ class ReportActivityUsersController < ApplicationController
 
   def update
     @report_activity_user = ReportActivityUser.find(params[:id])
-    if @report_activity_user.update_attributes(params[:report_activity_user])
-      #redirect_to @report_activity_user, :notice  => "Successfully updated report activity user."
+    if @report_activity_user.update_attributes(params[:report_activity_user])      
       respond_to do |format|
-        #format.html redirect_to(@report_activity_user, :notice => "Successfully created report activity user.") 
+        format.html { redirect_to(@report_activity_user, :notice => "Successfully created report activity user.") }
         format.xml { render :xml => @report_activity_user.to_xml }
       end
     else
